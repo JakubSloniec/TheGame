6b00da5f7f5fd9fca3057167dbc79cdb980f6fab
Michal Mrowczyk <mrowczyk@agh.edu.pl>
2015-11-10 12:45:46 +0100

    Reading JSON configuration of game map

---
+++ src/main/java/parser/MapConfigReader.java
@@ -0,0 +1,57 @@
+package parser;
+
+/**
+ * Created by Michal Mrowczyk on 11/10/2015.
+ */
+
+import com.fasterxml.jackson.core.JsonGenerationException;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.io.File;
+import java.io.IOException;
+
+/**
+ * MapConfigReader class reads JSON file representing map configuration
+ */
+public class MapConfigReader {
+
+    private ObjectMapper mapper;
+
+    public MapConfigReader() {
+        mapper = new ObjectMapper();
+    }
+
+    public RawMap readMapConfig(String fileName) {
+        // Getting JSON resource representing game map.
+        ClassLoader classLoader = getClass().getClassLoader();
+        File file = new File(classLoader.getResource(fileName).getFile());
+
+        ObjectMapper mapper = new ObjectMapper();
+        RawMap gameMap = null;
+
+        try {
+            // Convert JSON string from file to RawMap object
+            gameMap = mapper.readValue(file, RawMap.class);
+        } catch (JsonGenerationException e) {
+            e.printStackTrace();
+        } catch (JsonMappingException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return gameMap;
+    }
+
+    public String prettyPrint(RawMap gameMap) {
+        String prettyGameMapString = null;
+        try {
+            prettyGameMapString = mapper.writerWithDefaultPrettyPrinter().writeValueAsString(gameMap);
+        } catch (JsonProcessingException e) {
+            e.printStackTrace();
+        }
+        return prettyGameMapString;
+    }
+}
---
+++ src/main/java/parser/RawRoom.java
@@ -0,0 +1,62 @@
+package parser;
+
+/**
+ * Created by Michal Mrowczyk on 11/10/2015.
+ */
+public class RawRoom {
+
+    private int id;
+    private int x;
+    private int y;
+    private String type;
+    private String precondition;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getPrecondition() {
+        return precondition;
+    }
+
+    public void setPrecondition(String precondition) {
+        this.precondition = precondition;
+    }
+
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("COORDS: " + "(" + getX() + ", " + getY() + ")");
+        sb.append(" TYPE: " + getType());
+        sb.append(" PRECONDITION: " + getPrecondition());
+        sb.append("\n");
+        return sb.toString();
+    }
+}
