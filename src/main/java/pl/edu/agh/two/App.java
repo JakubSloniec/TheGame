a7e277bd0eab6aea015ecc62cb248cc61b87091e
Pszczolka, Mateusz <Mateusz.Pszczolka@sabre.com>
2015-11-17 09:00:28 +0100

    add example spring configuration and application start point

--- src/main/java/pl/edu/agh/two/App.java
+++ src/main/java/pl/edu/agh/two/App.java
@@ -1,13 +1,15 @@
 package pl.edu.agh.two;
 
-/**
- * Hello world!
- *
- */
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+
 public class App 
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        final ApplicationContext applicationContext = new ClassPathXmlApplicationContext("applicationContext.xml");
+        final Test bean = applicationContext.getBean(Test.class);
+        System.out.println(bean.getTest2().getHelloMessage());
     }
 }
