728a5f64220d931ad02f0d68b9712d299325323b
Krzysztof Kicinger <kkrzysiu@poczta.onet.pl>
2015-11-24 09:19:06 +0100

    Map Init

--- src/main/java/pl/edu/agh/two/gui/controllers/Controller.java
+++ src/main/java/pl/edu/agh/two/gui/controllers/Controller.java
@@ -1,9 +1,10 @@
 package pl.edu.agh.two.gui.controllers;
 
 import org.springframework.stereotype.Component;
-
 import pl.edu.agh.two.domain.map.Map;
+import pl.edu.agh.two.domain.players.IPlayer;
 import pl.edu.agh.two.gui.views.RootFrame;
+import pl.edu.agh.two.parser.factories.MapFactory;
 
 /**
  * @author Jakub Sloniec
@@ -13,6 +14,8 @@ import pl.edu.agh.two.gui.views.RootFrame;
 @Component
 public class Controller {
 
+    private Map map;
+    private IPlayer player;
 	private RootFrame rootFrame;
 
 	public void init() {
@@ -20,8 +23,14 @@ public class Controller {
 		rootFrame.setVisible(true);
 
 		rootFrame.getInputPanel().getBtnEnter().addActionListener(a -> clickEnter());
-		// displayMap();
+
+        initMap();
 	}
+
+    private void initMap() {
+        map = MapFactory.getMap();
+        displayMap(map);
+    }
 
 	public void clickEnter() {
 		String input = getInputText();
