40ae9923d327292725d6dfef1eb12bc1077c6984
zeuko <zuaskrzynka@gmailcom>
2015-11-27 19:30:41 +0100

    added tooltips for inventory items

--- src/main/java/pl/edu/agh/two/gui/views/InventoryPanel.java
+++ src/main/java/pl/edu/agh/two/gui/views/InventoryPanel.java
@@ -3,16 +3,22 @@ package pl.edu.agh.two.gui.views;
 import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Component;
+import java.util.HashMap;
 import java.util.Map;
 import java.util.Map.Entry;
+import java.util.Optional;
+import java.util.Set;
 
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.border.MatteBorder;
 
 import net.miginfocom.swing.MigLayout;
+import pl.edu.agh.two.domain.attributes.ItemAttribute;
+import pl.edu.agh.two.domain.items.AbstractAttributeItem;
 import pl.edu.agh.two.domain.items.IItem;
 import pl.edu.agh.two.domain.players.Backpack;
+import pl.edu.agh.two.factories.ItemsFactory;
 
 /**
  * @author Jakub Sloniec
@@ -30,6 +36,20 @@ public class InventoryPanel extends JPanel {
 	public InventoryPanel() {
 		setLayout(new BorderLayout(0, 0));
 		itemList = createItemPanel();
+		mock();
+	}
+
+	private void mock() {
+		Backpack bp = Backpack.createBackpack();
+		Map<String, Double> beerAttributes = new HashMap<String, Double>();
+		beerAttributes.put("si³a", (double) 10);
+		beerAttributes.put("moc", (double) 15);
+		IItem item = ItemsFactory.createPernamentItem("Piwo", Optional.of(beerAttributes), Optional.empty());
+		bp.addItem(item);
+		bp.addItem(item);
+		bp.addItem(item);
+		
+		paint(bp);
 	}
 
 	private JPanel createItemPanel() {
@@ -41,20 +61,48 @@ public class InventoryPanel extends JPanel {
 	public void paint(Backpack backpack) {
 		itemList = createItemPanel();
 		Map<IItem, Integer> items = backpack.getItems();
-		int row = 0;
+		int row = 1;
 		for (Entry<IItem, Integer> entry : items.entrySet()) {
 			IItem item = entry.getKey();
 			Integer amount = entry.getValue();
+			JPanel iconPanel = getItemIconPanel(item.getIconName());
+			JPanel namePanel = getItemNamePanel(item.getName());
+			JPanel amountPanel = getItemAmountPanel(amount);
+
+			if (item instanceof AbstractAttributeItem) {
+				String tooltipText = generateTooltip((AbstractAttributeItem)item, amount);
+				iconPanel.setToolTipText(tooltipText);
+				namePanel.setToolTipText(tooltipText);
+				amountPanel.setToolTipText(tooltipText);
+			}
+			
+			addCell(iconPanel, ICON_COLUMN, row);
+			addCell(namePanel, ITEM_COLUMN, row);
+			addCell(amountPanel, AMOUNT_COLUMN, row);
+			
 			row = row + 1;
-			addCell(getItemIconPanel(item.getIconName()), ICON_COLUMN, row);
-			addCell(getItemNamePanel(item.getName()), ITEM_COLUMN, row);
-			addCell(getItemAmountPanel(amount), AMOUNT_COLUMN, row);
 		}
 
 		add(itemList);
 	}
 
-	private Component getItemAmountPanel(Integer amount) {
+	private String generateTooltip(AbstractAttributeItem item, Integer amount) {
+		String tooltip = "<html>Ten przedmiot zmienia twoje statystyki! <br/>";
+		Set<ItemAttribute> attributes = item.getAttributes();
+		for (ItemAttribute itemAttribute : attributes) {
+			Double changeValue = itemAttribute.getChangeValue();
+			if (changeValue < 0) {
+				tooltip = tooltip + "<br/>- ";
+			} else {
+				tooltip = tooltip + "<br/>+ ";
+			}
+			tooltip = tooltip + amount*changeValue + " " + itemAttribute.getAttribute().getName();
+		}
+		
+		return tooltip + "</html>";
+	}
+
+	private JPanel getItemAmountPanel(Integer amount) {
 		JPanel panel = new JPanel();
 		panel.setBorder(new MatteBorder(1, 1, 1, 1, (Color) new Color(0, 0, 0)));
 		JLabel jlabel = new JLabel(amount.toString());
@@ -62,7 +110,7 @@ public class InventoryPanel extends JPanel {
 		return panel;
 	}
 
-	private Component getItemNamePanel(String name) {
+	private JPanel getItemNamePanel(String name) {
 		JPanel panel = new JPanel();
 		panel.setBorder(new MatteBorder(1, 1, 1, 1, new Color(0, 0, 0)));
 		JLabel jlabel = new JLabel(name);
@@ -70,7 +118,7 @@ public class InventoryPanel extends JPanel {
 		return panel;
 	}
 
-	private Component getItemIconPanel(String iconName) {
+	private JPanel getItemIconPanel(String iconName) {
 		JPanel panel = new JPanel();
 		panel.setBorder(new MatteBorder(1, 1, 1, 1, new Color(0, 0, 0)));
 		panel.setBackground(Color.BLUE);
