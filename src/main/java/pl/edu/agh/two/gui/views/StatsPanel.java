f38b5a793eed93334cf7c6baebfd761971a5afa4
zeuko <zuaskrzynka@gmailcom>
2015-11-27 18:55:47 +0100

    added stats panel

--- src/main/java/pl/edu/agh/two/gui/views/StatsPanel.java
+++ src/main/java/pl/edu/agh/two/gui/views/StatsPanel.java
@@ -1,10 +1,14 @@
 package pl.edu.agh.two.gui.views;
 
-import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Component;
 import java.util.Set;
 
+import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.border.MatteBorder;
 
+import net.miginfocom.swing.MigLayout;
 import pl.edu.agh.two.domain.players.statistics.IPlayerStatistic;
 
 /**
@@ -14,13 +18,63 @@ import pl.edu.agh.two.domain.players.statistics.IPlayerStatistic;
 @SuppressWarnings("serial")
 public class StatsPanel extends JPanel {
 
+	private static final Integer HEIGHT = 20;
+	private JPanel statList;
+
 	public StatsPanel() {
-		setLayout(new BorderLayout(0, 0));
+		statList = createStatPanel();
+	}
+
+	private JPanel createStatPanel() {
+		JPanel statList = new JPanel();
+		statList.setLayout(new MigLayout("", "", ""));
+		return statList;
 	}
 
 	public void paint(Set<IPlayerStatistic> statistics) {
-		
-		
+		statList = createStatPanel();
+		int row = 0;
+		for (IPlayerStatistic stat : statistics) {
+			row = row + 1;
+			String attributeName = stat.getAttribute().getName();
+			Double currentValue = stat.getCurrentValue();
+			addCell(getStatNameCell(attributeName), 1, row);
+			addCell(getStatValueCell(currentValue), 2, row);
+		}
+
+		add(statList);
 	}
 
+	private Component getStatValueCell(Double name) {
+		JPanel panel = new JPanel();
+		panel.setBorder(new MatteBorder(1, 1, 1, 1, new Color(0, 0, 0)));
+		JLabel jlabel = new JLabel(name.toString());
+		panel.add(jlabel);
+		return panel;
+	}
+
+	private Component getStatNameCell(String statName) {
+		JPanel panel = new JPanel();
+		panel.setBorder(new MatteBorder(1, 1, 1, 1, new Color(0, 0, 0)));
+		JLabel jlabel = new JLabel(statName);
+		panel.add(jlabel);
+		return panel;
+	}
+
+	private void addCell(Component component, int column, int row) {
+		statList.add(component, "cell " + column + " " + row + ", width :" + checkWidth(column) + ":, height :" + HEIGHT + ":");
+	}
+
+	private int checkWidth(int column) {
+		switch (column) {
+		case 1:
+			return 200;
+		case 2:
+			return 40;
+
+		default:
+			break;
+		}
+		throw new RuntimeException("No column width configuration for stat panel column " + column);
+	}
 }
