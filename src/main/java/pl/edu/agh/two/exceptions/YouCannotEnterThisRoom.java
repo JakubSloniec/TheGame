5748bf218fdc9a8e66ef64467032d6f5e6f3e399
Mateusz <mateusz.pszczolka@gmail.com>
2015-12-23 14:10:56 +0100

    Update YouCannotEnterThisRoom.java

--- src/main/java/pl/edu/agh/two/exceptions/YouCannotEnterThisRoom.java
+++ src/main/java/pl/edu/agh/two/exceptions/YouCannotEnterThisRoom.java
@@ -1,8 +1,5 @@
 package pl.edu.agh.two.exceptions;
 
-/**
- * Created by ps_krzysztof on 2015-11-16.
- */
 public class YouCannotEnterThisRoom extends Exception {
     public YouCannotEnterThisRoom(String message) {
         super(message);
