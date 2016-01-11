b386bb2d946fea1322ab212056979766f04880ea
Pszczolka, Mateusz <Mateusz.Pszczolka@sabre.com>
2016-01-11 23:54:07 +0100

    - allow to use USE command in combat

--- src/main/java/pl/edu/agh/two/domain/rooms/EmptyRoom.java
+++ src/main/java/pl/edu/agh/two/domain/rooms/EmptyRoom.java
@@ -2,18 +2,7 @@ package pl.edu.agh.two.domain.rooms;
 
 import pl.edu.agh.two.domain.players.IPlayer;
 
-/**
- * Created by ps_krzysztof on 2015-11-16.
- */
 public class EmptyRoom extends AbstractRoom {
-
-    public static class Builder extends AbstractRoom.Builder<EmptyRoom> {
-
-        public Builder(Coordinates coordinates) {
-            super(new EmptyRoom(coordinates));
-        }
-
-    }
 
     public EmptyRoom(Coordinates coordinates) {
         super(coordinates);
@@ -25,6 +14,14 @@ public class EmptyRoom extends AbstractRoom {
 
     @Override
     public void executeEvent(IPlayer player) {
+    }
+
+    public static class Builder extends AbstractRoom.Builder<EmptyRoom> {
+
+        public Builder(Coordinates coordinates) {
+            super(new EmptyRoom(coordinates));
+        }
+
     }
 
 }
